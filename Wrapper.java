e54794076cfc5ee5d3ae52c56da3a1f43a079923
Naga-himaja-reddy <somalanagahimaja@gmail.com>
2025-06-04 15:58:26 +0530

    Add files via upload

---
+++ Wrapper.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Wrapper
+{
+	public static void main(String[] args) {
+	String a= new String("bijen singha");
+    for(int i=0;i<a.length();i++)
+    {
+        System.out.println(a.charAt(i));
+    }
+	}
+}
