c6e253551c309fa98895c732e2f62ef52865186d
Naga-himaja-reddy <somalanagahimaja@gmail.com>
2025-06-04 15:50:39 +0530

    Add files via upload

---
+++ Mininum.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Minimum
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+		int[] a=new int[5];
+		for(int i=0;i<a.length;i++)
+		{
+		    a[i]=sc.nextInt();
+		}
+	int m=a[0];
+	for(int i=0;i<a.length;i++)
+	{
+	    if(a[i]<m)
+	    m=a[i];
+	}
+	System.out.println(m);
+}
+}
