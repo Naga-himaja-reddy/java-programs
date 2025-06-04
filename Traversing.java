c7a5371fdaad4ead559813a3d1776c4d1e58ba90
Naga-himaja-reddy <somalanagahimaja@gmail.com>
2025-06-04 15:55:45 +0530

    Add files via upload

---
+++ Traversing.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Traversing
+{
+	public static void main(String[] args) {
+	String a="bijen singha";
+    for(int i=0;i<a.length();i++)
+    {
+        System.out.println(a.charAt(i));
+    }
+	}
+}
